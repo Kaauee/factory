6d455157187855788eb9df370b34a43778f6c161
KaueLuiz21 <kaueluiz@outlook.com.br>
2022-05-12 15:43:09 -0300

    Padrao criacional Factory

---
+++ src/Formas/Rectangle.java
@@ -0,0 +1,12 @@
+package Formas;
+
+public class Rectangle implements Shape{
+
+    @Override
+    public void draw() {
+        System.out.println("Dentro do metodo de desenhar() do retangulo");
+    }
+
+
+
+}
---
+++ src/Formas/Square.java
@@ -0,0 +1,12 @@
+package Formas;
+
+public class Square implements Shape{
+
+    @Override
+    public void draw() {
+       System.out.println("Dentro do metodo de desenhar() do quadrado");
+    }
+
+
+
+}
